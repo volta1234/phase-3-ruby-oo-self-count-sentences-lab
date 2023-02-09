9a65eb35e2f388d11dd80003a6b6ec0ce6b6adc2
CALVINCE <calvince.ogwel@gmail.com>
2023-02-09 20:21:08 +0300

    completed count sentences lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+   if self.end_with?(".")
+      return true
+   else return false
+   end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+   else return false
+   end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+   else return false
+   end
   end
 
   def count_sentences
-
+      self.split(/[.!?]+/).length
   end
 end
+
+puts "calvince is a programmer. are you a programmer? you are doomed!".count_sentences
